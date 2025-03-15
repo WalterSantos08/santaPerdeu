4d002e5c293ed83f91db80efd2f7921d5664ecfc
Walter Santos <waltersantosfilho94@gmail.com>
2025-03-15 09:50:53 -0300

    Update perdeu.js

--- perdeu.js
+++ perdeu.js
@@ -1,25 +1,29 @@
-function showElement(selector) {
-    document.querySelector(selector).style.opacity = '1';
-    document.querySelector(selector).style.translate = '0';
+function showElements(selector) {
+    document.querySelectorAll(selector).forEach(element => {
+        element.style.opacity = '1';
+        element.style.translate = '0';
+    });
 }
 
 function showUp(selector) {
-    document.querySelector(selector).style.opacity = '1';
+    document.querySelectorAll(selector).forEach(element => {
+        element.style.opacity = '1';
+    });
 }
 
 function animation() {
-    showElement('h1');
+    showElements('h1');
 
-    setTimeout(() => showElement('h2'), 300);
-    setTimeout(() => showElement('.att1'), 500);
-    setTimeout(() => showElement('.att2'), 1000);
-    setTimeout(() => showElement('.att3'), 1500);
-    setTimeout(() => showElement('.att4'), 2000);
+    setTimeout(() => showElements('h2'), 300);
+    setTimeout(() => showElements('.att1'), 500);
+    setTimeout(() => showElements('.att2'), 1000);
+    setTimeout(() => showElements('.att3'), 1500);
+    setTimeout(() => showElements('.att4'), 2000);
     setTimeout(() => showUp('.up1'), 3000);
     setTimeout(() => showUp('.up2'), 4000);
     setTimeout(() => showUp('.up3'), 5000);
     setTimeout(() => showUp('.up4'), 6000);
-    setTimeout(() => showElement('button'), 7000);
+    setTimeout(() => showElements('button'), 7000); // Agora afeta todos os botões
 }
 
 setTimeout(animation, 100);
